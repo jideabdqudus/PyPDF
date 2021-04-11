a6713367b725ec444f3f6f043205b9e32e6050e5
jideabdqudus <jideabdqudus@gmail.com>
2021-04-11 22:09:56 +0100

    +

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import PyPDF2
+import sys
 
+"""
 with open("dummy.pdf", "rb") as file:
     reader = PyPDF2.PdfFileReader(file)
     print(reader.numPages)
     print(reader.getPage(0))
+"""
+
+inputs = sys.argv[1:]
+
+
+def combiner(pdf_list):
+    for pdf in pdf_list:
+        print(pdf)
+
+
+combiner(inputs)
